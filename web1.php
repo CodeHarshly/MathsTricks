814a1e211ab9e22d875d7faed9715c19bbba4a14
harshdeepsinghchoudhary <137483476+harshdeepsinghchoudhary@users.noreply.github.com>
2023-09-07 16:25:08 +0530

    Add files via upload

---
+++ web1.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<div class="bg"></div>
+  <div class="star">
+    <!-- Your webpage content here -->
+  </div>
+
+</body>
+</html>
